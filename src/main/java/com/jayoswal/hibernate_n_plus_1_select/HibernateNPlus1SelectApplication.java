670c52a46d87b12ee3ac4eb52fe8b8a343446204
jayoswal <jayumeshoswal2001@gmail.com>
2024-07-02 16:10:17 +0530

    Basic Spring boot project - web, data jpa, mysql, lombok

---
+++ src/main/java/com/jayoswal/hibernate_n_plus_1_select/HibernateNPlus1SelectApplication.java
@@ -0,0 +1,13 @@
+package com.jayoswal.hibernate_n_plus_1_select;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernateNPlus1SelectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernateNPlus1SelectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jayoswal/hibernate_n_plus_1_select/HibernateNPlus1SelectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jayoswal.hibernate_n_plus_1_select;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernateNPlus1SelectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
